214b8098e2c9615d7c72835bc1dc18c43a55a26c
Dixita Ganatra <contact@dixita.dev>
2023-12-26 16:17:57 +0530

    Add print resume button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <NavLink to="/" aria-label="Home">
+    <NavLink to="/" aria-label="Home" className={styles.header}>
       <h1 className={styles.title}>Resume Builder</h1>
     </NavLink>
   );
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,9 +1,21 @@
 import React from "react";
 import OnePage from "./themes/OnePage/OnePage";
+import styles from "./Preview.module.css";
 
 export default function Preview({ data, theme }) {
+  let preview = null;
+
   switch (theme) {
     default:
-      return <OnePage resume={data} />;
+      preview = <OnePage resume={data} />;
   }
+
+  return (
+    <section className={styles.container}>
+      <button type="button" onClick={() => window.print()}>
+        Print
+      </button>
+      {preview}
+    </section>
+  );
 }
